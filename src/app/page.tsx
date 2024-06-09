eaa6cc2aee282954c7690f2c24141c3674cf86a1
Beatriz Maia <prbeatrizmaia@gmail.com>
2024-06-09 00:02:47 -0300

    revert test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <h2>Compre Nas <span className="text-blueMarine-700">Principais Categorias</span></h2>
         </div>
         <div className="w-fit font-semibold text-gray-500 border-b-2 border-solid border-blueMarine-700">
-          <h2>Top <span className="text-blueMarine-700">Marcas de Eletronicos.</span></h2>
+          <h2>Top <span className="text-blueMarine-700">Marcas de Eletronicos</span></h2>
         </div>
       </div>
     </main>
